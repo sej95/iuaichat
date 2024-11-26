059a8dfe54f5351aa285f8ada80e406beffe47d3
canisminor1990 <i@canisminor.cc>
2024-11-27 01:46:04 +0800

    💄 style: Update changelog style

--- next.config.mjs
+++ next.config.mjs
@@ -103,24 +103,6 @@ const nextConfig = {
       },
     ];
   },
-
-  images: {
-    remotePatterns: [
-      {
-        hostname: 'localhost',
-        protocol: 'http',
-      },
-      {
-        hostname: '127.0.0.1',
-        protocol: 'http',
-      },
-      {
-        hostname: '**',
-        protocol: 'https',
-      },
-    ],
-    // unoptimized: true,
-  },
   output: buildWithDocker ? 'standalone' : undefined,
   reactStrictMode: true,
   redirects: async () => [
