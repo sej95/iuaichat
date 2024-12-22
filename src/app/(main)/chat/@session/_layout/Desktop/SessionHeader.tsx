b6ddfa8ca5ab820e5fac54d2855bc08a7fd324d2
sej95 <144600333+sej95@users.noreply.github.com>
2024-12-22 20:58:33 +0800

    Update SessionHeader.tsx

--- src/app/(main)/chat/@session/_layout/Desktop/SessionHeader.tsx
+++ src/app/(main)/chat/@session/_layout/Desktop/SessionHeader.tsx
@@ -39,7 +39,7 @@ const Header = memo(() => {
     <Flexbox className={styles.top} gap={16} padding={16}>
       <Flexbox distribution={'space-between'} horizontal>
         <Flexbox align={'center'} gap={4} horizontal>
-          <ProductLogo className={styles.logo} size={36} type={'text'} />
+          <ProductLogo className={styles.logo} size={0} type={'text'} />
           {enableWebrtc && <SyncStatusTag />}
         </Flexbox>
         {showCreateSession && (
